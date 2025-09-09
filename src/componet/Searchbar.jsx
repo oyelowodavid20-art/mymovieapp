8bb18fa30697455a38273f1f0f1b7d1f5c2c20a0
david <oyelowodavid20@gmail.com>
2025-09-09 16:15:51 +0100

    styled search bar componet

--- src/componet/Searchbar.jsx
+++ src/componet/Searchbar.jsx
@@ -1,8 +1,8 @@
 import { Search } from "lucide-react";
 export default function Searchbar() {
   return (
-    <div className="flex p-4 gap-2 ">
-        <input type="text" className="w-full border"/>
+    <div className="flex p-4 gap-2  items-center ">
+        <input type="text" className="w-full border p-2 rounded bg-gray-300 outline-none"/>
         <Search />
     </div> 
   );
