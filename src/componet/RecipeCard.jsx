381d6f0d7cfd708b298ff542b6363d75c4932bea
david <oyelowodavid20@gmail.com>
2025-09-10 14:11:31 +0100

    designed image

--- src/componet/RecipeCard.jsx
+++ src/componet/RecipeCard.jsx
@@ -1,7 +1,11 @@
 export default function RecipeCard() {
   return (
-    <div className="w-[80%] mx-auto showdow-2xl">
-      <img src="./download.jpeg" alt="strawberry" className="w-full h-[300px] object-center object-fit" />
+    <div className="w-[80%] mx-auto shadow-2xl">
+      <img
+        src="./download.jpeg"
+        alt="strawberry"
+        className="w-full h-[300px] object-center object-fit"
+      />
       <div className="p-6">
         <h2>Name:strawberrry</h2>
         <h2>Rating:2.5</h2>
