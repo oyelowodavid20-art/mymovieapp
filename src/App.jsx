2f12f6c80856caa59db28f3db66599bb660b1703
david <oyelowodavid20@gmail.com>
2025-09-10 12:33:18 +0100

    styled the container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Header from "./componet/Header"
-import Searchbar from "./componet/Searchbar"
+import Card from "./componet/Card";
+import Header from "./componet/Header";
+import Searchbar from "./componet/Searchbar";
 function App() {
   return (
-<div>
-  <Header/>
-  <Searchbar/>
-</div>
-  )
+    <div className="border-2 w-72 h-72 items-center m-10">
+      <Header />
+      <Searchbar />
+      <Card />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/componet/Searchbar.jsx
+++ src/componet/Searchbar.jsx
@@ -3,7 +3,7 @@ export default function Searchbar() {
   return (
     <div className="flex p-4 gap-2  items-center ">
         <input type="text" className="w-full border p-2 rounded bg-gray-300 outline-none"/>
-        <Search />
+       <button> <Search /> </button>
     </div> 
   );
 }
