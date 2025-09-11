86ed75230cb8b7fc72929a7246c531d95bb6f2f5
david <oyelowodavid20@gmail.com>
2025-09-11 12:41:18 +0100

    styled header

--- src/componet/Header.jsx
+++ src/componet/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h2 className="flex justify-between p-6 shadow-md font-bold text-xl items-center  ">
+    <h2 className="flex justify-between p-6 shadow-md font-bold text-xl items-center  fixed top-0 w-full  fill-white bg-white">
       Food
       <button className="flex flex-col gap-1">
         <span className="bg-black w-4 h-1 inline-block"></span>
--- src/componet/Searchbar.jsx
+++ src/componet/Searchbar.jsx
@@ -1,8 +1,8 @@
 import { Search } from "lucide-react";
 export default function Searchbar() {
   return (
-    <div className="flex p-4 gap-2  mb-4  ">
-        <input type="text" className="w-full border p-2 rounded bg-gray-300"/>
+    <div className="flex p-5 items-center gap-6 ">
+        <input type="text" className="w-full border p-2 rounded bg-gray-300 my-16 outline-none"/>
         <Search />
     </div> 
   );
